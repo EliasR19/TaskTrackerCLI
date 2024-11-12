7bf4b7193c44dad02ad281dd02e7571536afba39
EliasR19 <ereynso2040@gmail.com>
2024-11-12 16:38:49 -0300

    Prueba de commit a gitHub desde Eclipse

---
+++ src/no_se/BorrarDespues.java
@@ -0,0 +1,10 @@
+package no_se;
+
+public class BorrarDespues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, el inicio de las tareas :)");
+	}
+
+}
